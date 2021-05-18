c41356443558d054c01fa428474feb0c94dde85a
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 22:54:28 -0500

    Se agrega método para consultar todos los datos de CURSO_TEMA_VIDEO.txt

--- CURSO_TEMA_VIDEO.py
+++ CURSO_TEMA_VIDEO.py
@@ -12,3 +12,8 @@ class CURSO_TEMA_VIDEO:
         f.write(str(self.__id_CT)+'|')
         f.write(str(self.__id_video)+ '\n')
         f.close()
+
+    def consultar_todo(self):
+        f = open("c:\Programas\CURSO_TEMA_VIDEO.txt")
+        print(f.read())
+        f.close()
