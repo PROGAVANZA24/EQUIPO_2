95a0ab5caee2d63e8505f21b74d4fae3ffc14288
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 20:21:17 -0500

    Se agrega el método para guardar los datos en un txt

--- TEMA.py
+++ TEMA.py
@@ -3,3 +3,9 @@ class TEMA:
     def __init__(self, id_tema, nombre):
         self.__id_tema = id_tema
         self.__nombre = nombre
+
+    def guardar(self):
+        f = open("c:\Programas\TEMA.txt","a", encoding="utf8")
+        f.write(str(self.__id_tema)+'|')
+        f.write(str(self.__nombre)+'\n')
+        f.close()
