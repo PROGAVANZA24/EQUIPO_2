88385b70b5027276fdaccfa06e0d03c646a9c77e
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 23:23:53 -0500

    Se importan las clases de los distintos py

---
+++ PROGRAMA_PRINCIPAL.py
@@ -0,0 +1,6 @@
+from TEMA import TEMA
+from CURSO import CURSO
+from VIDEO import VIDEO
+from EMPLEADO import EMPLEADO
+from CURSO_TEMA import CURSO_TEMA
+from CURSO_TEMA_VIDEO import CURSO_TEMA_VIDEO
