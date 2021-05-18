d341ec7e26144778f2b49cddeec320026f111772
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 21:13:27 -0500

    Se agrega la clase CURSO y sus matributos de instancia

---
+++ CURSO.py
@@ -0,0 +1,5 @@
+class CURSO:
+    def __init__(self, id_curso, descripcion, id_empleado):
+        self.__id_curso = id_curso
+        self.__descripcion = descripcion
+        self.__id_empleado = id_empleado
