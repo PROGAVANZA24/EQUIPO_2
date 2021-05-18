26237dba311321439f1b25376bfd102b402ffaff
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 23:38:45 -0500

    Se agrega primera vista para el usuario, así como también una estructura de repetición.

--- PROGRAMA_PRINCIPAL.py
+++ PROGRAMA_PRINCIPAL.py
@@ -4,3 +4,13 @@ from VIDEO import VIDEO
 from EMPLEADO import EMPLEADO
 from CURSO_TEMA import CURSO_TEMA
 from CURSO_TEMA_VIDEO import CURSO_TEMA_VIDEO
+
+x=1
+while True:
+    print ("**MENU DE ADMINISTRADOR**")
+    x= int(input("¿Quieres seguir realizando acciones? (1 para seguir - *otro número* para salir): "))
+    if x != 1:
+        break
+
+
+
