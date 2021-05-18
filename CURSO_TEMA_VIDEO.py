6915cb5078c347448677e5c14485c59949cf95ba
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 22:48:43 -0500

    Se agregó la clase CURSO_TEMA_VIDEO y se agregaron sus atributos de instancia

---
+++ CURSO_TEMA_VIDEO.py
@@ -0,0 +1,7 @@
+class CURSO_TEMA_VIDEO:
+
+    def __init__(self, id_CTV, id_CT, id_video):
+        
+        self.__id_CTV = id_CTV
+        self.__id_CT = id_CT
+        self.__id_video = id_video
