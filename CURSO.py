612773599f8702e56da87d212c13308e83fd8fdd
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 21:30:16 -0500

    Se agrega el método para guardar los datos en el archivo CURSO

--- CURSO.py
+++ CURSO.py
@@ -3,3 +3,10 @@ class CURSO:
         self.__id_curso = id_curso
         self.__descripcion = descripcion
         self.__id_empleado = id_empleado
+        
+    def guardar(self):
+        f = open("c:\Programas\CURSO.txt","a", encoding="utf8")
+        f.write(str(self.__id_curso)+'|')
+        f.write(str(self.__descripcion)+'|')
+        f.write(str(self.__id_empleado)+ '\n')
+        f.close()
