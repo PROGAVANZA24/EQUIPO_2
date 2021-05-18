925fb7406c9192f6546805090d2bb452ab990dad
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 21:42:55 -0500

    Se agrega el método para guardar los datos de EMPLEADO

--- EMPLEADO.py
+++ EMPLEADO.py
@@ -3,3 +3,10 @@ class EMPLEADO:
         self.__id_empleado = id_empleado
         self.__nombre = nombre
         self.__direccion =direccion
+
+    def guardar(self):
+        f = open("c:\Programas\EMPLEADO.txt","a", encoding="utf8")
+        f.write(str(self.__id_empleado)+'|')
+        f.write(str(self.__nombre)+'|')
+        f.write(str(self.__direccion)+ '\n')
+        f.close()
