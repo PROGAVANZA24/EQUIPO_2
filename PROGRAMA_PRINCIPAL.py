294c486f316fc135facf9931a8700cd390f5f0fb
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 23:55:14 -0500

    Se agrega el primer uso de cla clase TEMA y con elllo se usa un método

--- PROGRAMA_PRINCIPAL.py
+++ PROGRAMA_PRINCIPAL.py
@@ -8,6 +8,14 @@ from CURSO_TEMA_VIDEO import CURSO_TEMA_VIDEO
 x=1
 while True:
     print ("**MENU DE ADMINISTRADOR**")
+    des = int(input("\nElige la base de datos a trabajar.\n1-TEMA. 2-CURSO. 3-VIDEO. 4-EMPLEADO. 5-CURSO_TEMA. 6-CURSO_TEMA_VIDEO. : "))
+    opc = int(input("\nElige la acción que quieres realizar.\n1-Insertar datos. 2-Revisar todos los datos. 3.Consultar datos por ID: "))
+    if des == 1:
+        if opc == 1:
+            a = input("Escribe el id del tema: ")
+            b = input("Escribe el nombre del tema: ")
+            info = TEMA(a,b)
+            info.guardar()
     x= int(input("¿Quieres seguir realizando acciones? (1 para seguir - *otro número* para salir): "))
     if x != 1:
         break
