f6a1bcefae2c744f4e07b49fa8241c9e5bab0016
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 21:40:04 -0500

    Se agrega la clase EMPLEADO y sus atributos de instancia

---
+++ EMPLEADO.py
@@ -0,0 +1,5 @@
+class EMPLEADO:
+    def __init__(self, id_empleado, nombre, direccion):
+        self.__id_empleado = id_empleado
+        self.__nombre = nombre
+        self.__direccion =direccion
