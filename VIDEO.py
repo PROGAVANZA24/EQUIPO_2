d68c607bac159e6ac0e2dc28088b24ef089e4c0b
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 20:36:07 -0500

    Se crea la clase y se agregan los atributos de instancia.

---
+++ VIDEO.py
@@ -0,0 +1,6 @@
+class VIDEO:
+    def __init__(self, id_video, nombre, url, fecha_publicacion):
+        self.__id_video = id_video
+        self.__nombre = nombre
+        self.__url = url
+        self.__fecha_publicacion = fecha_publicacion
