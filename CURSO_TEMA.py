3de9a7f6a3c0b7c4cbdd114f624f06a9c40f8b13
Antonio Alexander Soto Molina <tonni_sm@hotmail.com>
2021-05-17 21:59:06 -0500

    Se agrega método para guardar los datos en CURSO_TEMA.txt

--- CURSO_TEMA.py
+++ CURSO_TEMA.py
@@ -3,3 +3,12 @@ class CURSO_TEMA:
         self.__id_CT = id_CT
         self.__id_curso = id_curso
         self.__id_tema = id_tema
+
+    def guardar(self):
+        f = open("c:\Programas\CURSO_TEMA.txt","a", encoding="utf8")
+        f.write(str(self.__id_CT)+'|')
+        f.write(str(self.__id_curso)+'|')
+        f.write(str(self.__id_tema)+ '\n')
+        f.close()
+
+    
